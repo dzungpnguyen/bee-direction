e478557028bc50b7a733ce61f4a6d2f887b11c0d
Dung Nguyen <thienhoangphuongdung@gmail.com>
2025-09-18 22:42:13 +0200

    split

--- src/train_val_split.py
+++ src/train_val_split.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import shutil
+import random
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
@@ -27,16 +28,30 @@ if train_percent < .01 or train_percent > 0.99:
     sys.exit(0)
 
 # create output paths, going up to 2 level
-# example:  data / detection_N / train
-#           data / detection_N / validation
+# example:  data / segmentation_N / train
+#           data / segmentation_N / validation
 train_image_dir = data_dir.parent.parent / data_dir.name / 'train' / 'images'
 train_label_dir = data_dir.parent.parent / data_dir.name / 'train' / 'labels'
 val_image_dir = data_dir.parent.parent / data_dir.name / 'validation' / 'images'
 val_label_dir = data_dir.parent.parent / data_dir.name / 'validation' / 'labels'
 dirs = [train_image_dir, train_label_dir, val_image_dir, val_label_dir]
 for dir in dirs:
+    # delete if folder exists
     if dir.exists() and dir.is_dir():
-        # delete if folder exists
         shutil.rmtree(dir)
     dir.mkdir(mode=0o777, parents=True, exist_ok=False)
 
+images = [path for path in data_dir.rglob("*") if path.suffix.lower() == '.jpg']
+n_train = int(train_percent * len(images))
+train_images = random.sample(images, n_train)
+val_images = [image for image in images if image not in train_images]
+train_labels = [(image.parent.parent / 'labels' / image.name).with_suffix('.txt') for image in train_images]
+val_labels = [(image.parent.parent / 'labels' / image.name).with_suffix('.txt') for image in val_images]
+assert len(train_images) == len(train_labels)
+assert len(val_images) == len(val_labels)
+for i in range (len(train_images)):
+    shutil.copy(train_images[i], train_image_dir / train_images[i].name)
+    shutil.copy(train_labels[i], train_label_dir / train_labels[i].name)
+for i in range (len(val_images)):
+    shutil.copy(val_images[i], val_image_dir / val_images[i].name)
+    shutil.copy(val_labels[i], val_label_dir / val_labels[i].name)
