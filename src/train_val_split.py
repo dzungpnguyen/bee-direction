c15ee0b31567b81a3f71878dea7379f0347c5a3f
Dung Nguyen <thienhoangphuongdung@gmail.com>
2025-09-18 22:59:40 +0200

    comments

--- src/train_val_split.py
+++ src/train_val_split.py
@@ -4,16 +4,16 @@ import shutil
 import random
 from pathlib import Path
 
+# pass input arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', help='Path to dataset', required=True)
-parser.add_argument('--train_pct', help='Ratio of train data, the rest go to validation (default value: .8)', default=.8)
+parser.add_argument('--train_pct', help='Ratio of train data, the rest go to validation (default value: .8)', type=float, default=.8)
 args = parser.parse_args()
 
 data_dir = Path(args.data_dir)
 train_percent = float(args.train_pct)
-# ouput_folder = args.output_folder
 
-# Check for valid entries
+# validate entries
 if not data_dir.is_dir():
     print('Dataset directory not found. Try again.')
     sys.exit(0)
@@ -24,10 +24,11 @@ if train_percent < .01 or train_percent > 0.99:
     print('Invalid train_pct. Expected value between .01 and .99.')
     sys.exit(0)
 
-print('Confirmed ataset path:', data_dir)
-print('Confirmed train ratio:', train_percent*100, '%')
 print('='*50)
+print('Confirmed dataset path:', data_dir)
+print('Confirmed train ratio:', train_percent*100, '%')
 
+print('='*50)
 # create output paths, going up to 2 level
 # example:  data / segmentation_N / train
 #           data / segmentation_N / validation
@@ -36,27 +37,34 @@ train_label_dir = data_dir.parent.parent / data_dir.name / 'train' / 'labels'
 val_image_dir = data_dir.parent.parent / data_dir.name / 'validation' / 'images'
 val_label_dir = data_dir.parent.parent / data_dir.name / 'validation' / 'labels'
 dirs = [train_image_dir, train_label_dir, val_image_dir, val_label_dir]
+# delete if folder exists, then create
 for dir in dirs:
-    # delete if folder exists
     if dir.exists() and dir.is_dir():
         shutil.rmtree(dir)
     dir.mkdir(mode=0o777, parents=True, exist_ok=False)
     print('Created folder', dir)
-print('='*50)
 
+print('='*50)
+# list all images
 images = [path for path in data_dir.rglob("*") if path.suffix.lower() == '.jpg']
+# number of train data
 n_train = int(train_percent * len(images))
+# train images (randomly chose) and val images
 train_images = random.sample(images, n_train)
 val_images = [image for image in images if image not in train_images]
+# corresponding labels
 train_labels = [(image.parent.parent / 'labels' / image.name).with_suffix('.txt') for image in train_images]
 val_labels = [(image.parent.parent / 'labels' / image.name).with_suffix('.txt') for image in val_images]
+# check size images = size labels
 assert len(train_images) == len(train_labels)
 assert len(val_images) == len(val_labels)
 print(f'Splitted into to {len(train_images)} train data and {len(val_images)} validation data')
+# copy to train folders
 for i in range (len(train_images)):
     shutil.copy(train_images[i], train_image_dir / train_images[i].name)
     shutil.copy(train_labels[i], train_label_dir / train_labels[i].name)
 print('Copied', len(train_images), 'train data')
+# copy to val folders
 for i in range (len(val_images)):
     shutil.copy(val_images[i], val_image_dir / val_images[i].name)
     shutil.copy(val_labels[i], val_label_dir / val_labels[i].name)
