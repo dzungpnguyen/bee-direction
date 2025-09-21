f5238942f837ed2cc30397ab9f58ccecd2dbdc3a
Dung Nguyen <thienhoangphuongdung@gmail.com>
2025-09-21 10:30:21 +0200

    refactor

---
+++ configs/variables.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+### global variables
+# works with the project structure: projet folder / sub folder / configs.py
+PROJECT_ROOT = Path(__file__).resolve().parents[1] if "__file__" in globals() else Path.cwd().parents[0]
+# number of used data
+SAMPLE_SIZE = 1000
+# name of the data folder
+DATA_FOLDER_NAME = f'segmentation_{SAMPLE_SIZE}'
